cb6b8653acf2be81471c5fc9aaa62e62971dfc64
David Salami <wizdave97@gmail.com>
2022-02-10 19:55:30 +0100

    add no_std

--- runtime-apis/src/lib.rs
+++ runtime-apis/src/lib.rs
@@ -12,6 +12,8 @@
 // GNU General Public License for more details.
 // You should have received a copy of the GNU General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
+#![cfg_attr(not(feature = "std"), no_std)]
+
 use codec::Codec;
 #[cfg(not(feature = "std"))]
 use sp_std::vec::Vec;
